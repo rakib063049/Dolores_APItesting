759b185612f35563b320dde80b47c9efe04a2ef3
rakib hasan <rakib063049@gmail.com>
2014-05-09 17:44:03 +0600

    Limited access for users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,5 +8,9 @@ class Ability
     else
       can :read, :all
     end
+
+    can :create, Customer if user.role?(:create_customers)
+    can :create, Contact if user.role?(:create_contacts)
+    can :manage, Project if user.role?(:create_projects)
   end
 end
