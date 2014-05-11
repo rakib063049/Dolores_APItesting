a5bbcc4cff1af03d761c93a01984cde619cf76d7
rakib hasan <rakib063049@gmail.com>
2014-05-11 09:28:12 +0600

    Valicate phone for contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,6 +6,7 @@ class Contact < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates :office_phone, presence: true
   validates :contact_method, presence: true
   #validates :customer_id, presence: true
   validate :validate_customer
