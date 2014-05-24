7af90557cd9f3a5b45d6c5bcb4961d173f8868a7
KCovell <KACovell@gmail.com>
2014-05-24 04:05:52 +0000

    Added Idle_time field to time log table

--- db/migrate/20140509182943_create_time_logs.rb
+++ db/migrate/20140509182943_create_time_logs.rb
@@ -3,6 +3,7 @@ class CreateTimeLogs < ActiveRecord::Migration
     create_table :time_logs do |t|
       t.datetime :start_at
       t.datetime :end_at
+      t.float :idle_time
       t.float :logged
       t.integer :user_id
       t.integer :project_id
